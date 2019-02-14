a58dd70b7679e4f563224f8e93f94fd58d4a9e73
Jongkeun <dldkfod@naver.com>
2019-02-14 13:33:57 +0900

    remove postcss-loader. Because I don't know what it is

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader'
-
-import FormContainer from "./js/components/container/FormContainer.jsx";
+import FormContainer from "./js/container/FormContainer";
 
 const wrapper = document.getElementById("root");
 wrapper ? ReactDOM.render(<FormContainer />, wrapper) : false;
