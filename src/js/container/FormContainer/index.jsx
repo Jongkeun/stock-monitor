6a31a8d84640bdfb6c1d237494fab93f893b0638
Jongkeun <dldkfod@naver.com>
2019-03-05 18:12:51 +0900

    make to search when pressing enter key.

--- src/js/container/FormContainer/index.jsx
+++ src/js/container/FormContainer/index.jsx
@@ -12,28 +12,40 @@ class FormContainer extends Component {
       price: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
   handleChange(event) {
     this.setState({ [event.target.id]: event.target.value });
-    getContent(event.target.value).then((content)=> {this.setState({ price: content });});
+    //getContent(event.target.value).then((content)=> {this.setState({ price: content });});
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    getContent(this.state.seo_title)
+      .then((content)=> {
+        this.setState({ price: content });
+      });
   }
   render() {
     const { seo_title, price } = this.state;
     return (
-      <form className="article-form">
-        <Input
-          id="seo_title"
-          text="SEO title"
-          label="seo_title"
-          type="text"
-          className="seo_title"
-          value={seo_title}
-          handleChange={this.handleChange}
-        />
+      <div>
+        <form className="article-form"  onSubmit={this.onSubmit}>
+          <Input
+            id="seo_title"
+            text="SEO title"
+            label="seo_title"
+            type="text"
+            className="seo_title"
+            value={seo_title}
+            handleChange={this.handleChange}
+          />
+        </form>
         <lable>
-          {price}
+            {price}
         </lable>
-      </form>
+      </div>
+
     );
   }
 }
